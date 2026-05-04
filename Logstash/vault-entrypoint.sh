86c9714559386af0ca3a281e58086a6f67c10eec
dlypian <dan.lypian@pantarhei.ca>
2026-05-04 14:13:01 -0600

    Changes to be committed: 	modified:   Logstash/docker-stack.yml 	modified:   Logstash/vault-entrypoint.sh

--- Logstash/vault-entrypoint.sh
+++ Logstash/vault-entrypoint.sh
@@ -7,36 +7,20 @@ echo "Fetching Logstash secrets from Vault..."
 : "${VAULT_TOKEN:?VAULT_TOKEN is required}"
 : "${VAULT_SECRET_PATH:?VAULT_SECRET_PATH is required}"
 
-NODE_BIN="/usr/share/logstash/jdk/bin/jshell"
+VAULT_JSON="$(curl -fsS \
+  -H "X-Vault-Token: ${VAULT_TOKEN}" \
+  "${VAULT_ADDR}/v1/${VAULT_SECRET_PATH}")"
 
-VAULT_JSON="$(ruby -r net/http -r uri -r json -e '
-  vault_addr = ENV.fetch("VAULT_ADDR")
-  vault_path = ENV.fetch("VAULT_SECRET_PATH")
-  vault_token = ENV.fetch("VAULT_TOKEN")
+json_get() {
+  local key="$1"
+  echo "$VAULT_JSON" | sed -n "s/.*\"${key}\":\"\\([^\"]*\\)\".*/\\1/p"
+}
 
-  uri = URI("#{vault_addr}/v1/#{vault_path}")
-  req = Net::HTTP::Get.new(uri)
-  req["X-Vault-Token"] = vault_token
-
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = uri.scheme == "https"
-
-  res = http.request(req)
-
-  unless res.code.to_i.between?(200, 299)
-    STDERR.puts "Vault request failed: #{res.code}"
-    STDERR.puts res.body
-    exit 1
-  end
-
-  puts res.body
-')"
-
-export ELASTIC_HOST="$(ruby -r json -e 'puts JSON.parse(ARGV[0]).dig("data","data","ELASTIC_HOST")' "$VAULT_JSON")"
-export ELASTIC_USER="$(ruby -r json -e 'puts JSON.parse(ARGV[0]).dig("data","data","ELASTIC_USER")' "$VAULT_JSON")"
-export ELASTIC_PASSWORD="$(ruby -r json -e 'puts JSON.parse(ARGV[0]).dig("data","data","ELASTIC_PASSWORD")' "$VAULT_JSON")"
-export CLIENT_DOMAIN="$(ruby -r json -e 'puts JSON.parse(ARGV[0]).dig("data","data","CLIENT_DOMAIN")' "$VAULT_JSON")"
-export ROOT_DOMAIN="$(ruby -r json -e 'puts JSON.parse(ARGV[0]).dig("data","data","ROOT_DOMAIN")' "$VAULT_JSON")"
+export ELASTIC_HOST="$(json_get ELASTIC_HOST)"
+export ELASTIC_USER="$(json_get ELASTIC_USER)"
+export ELASTIC_PASSWORD="$(json_get ELASTIC_PASSWORD)"
+export CLIENT_DOMAIN="$(json_get CLIENT_DOMAIN)"
+export ROOT_DOMAIN="$(json_get ROOT_DOMAIN)"
 
 if [[ -z "$ELASTIC_HOST" || "$ELASTIC_HOST" == "null" ]]; then
   echo "ELASTIC_HOST missing from Vault"
